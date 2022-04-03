b4bc00636e96b12747451589443df465c3a6d6d1
CherifiMi <cherifi154949@gmail.com>
2022-04-03 01:10:39 +0100

    Navigation / Dependence injection

--- app/src/main/java/com/example/pizzaorderapp/ui/screens/choose/ChooseScreen.kt
+++ app/src/main/java/com/example/pizzaorderapp/ui/screens/choose/ChooseScreen.kt
@@ -2,14 +2,20 @@ package com.example.pizzaorderapp.ui.screens.choose
 
 import android.util.Log
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectHorizontalDragGestures
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.consumeAllChanges
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.example.pizzaorderapp.navigation.Screens
 import com.example.pizzaorderapp.viewModel.MainViewModel
 
 @Composable
@@ -21,5 +27,22 @@ fun ChooseScreen(navController: NavHostController, mainViewModel: MainViewModel)
 
     Log.d("PIZZATEST", mainViewModel.allPizza.toString())
     Log.d("PIZZATEST", "hi")
+
+    Column(
+        verticalArrangement = Arrangement.Top,
+        modifier = Modifier
+            .clickable {
+                navController.navigate(route = Screens.Payment.passOrder(
+                    3,
+                    1400,
+                    "Mitooooooooooo"
+                ))
+            }
+            .background(Color.White)
+            .padding(32.dp, 16.dp)
+            .height(104.dp)
+            .fillMaxWidth()
+
+    ){}
 
 }
